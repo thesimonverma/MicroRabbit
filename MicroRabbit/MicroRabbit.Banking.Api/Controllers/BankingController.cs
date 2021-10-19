249dc5b595d9ce3607faacff93d00746ebb279fb
Simon Verma <thesimonverma@hotmail.com>
2021-10-19 13:07:03 +0530

    Partially comitted Banking microservice.

---
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,31 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BankingController : ControllerBase
+	{
+		private readonly IAccountService _accountService;
+
+		public BankingController(IAccountService accountService)
+		{
+			_accountService = accountService;
+		}
+
+		// GET: api/banking
+		[HttpGet]
+		public ActionResult<IEnumerable<Account>> Get()
+		{
+			return Ok(_accountService.GetAccounts());
+		}
+	}
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+	public abstract class Message : IRequest<bool>
+	{
+		public string Messagetype { get; protected set; }
+		protected Message()
+		{
+			Messagetype = GetType().Name;
+		}
+	}
+}
